62ef3a25bdfa9e0ead5bfceeaa56ca3086b93f21
TrollKiller1234 <pipivelky@gmail.com>
2024-12-09 12:13:07 +0100

    oprava chyby

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,15 +1,23 @@
-// src/components/ThemeProvider.tsx
 "use client";
 
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { ThemeProvider as MuiThemeProvider, createTheme } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 
+// Define the shape of the context data
+type ThemeContextType = {
+  toggleTheme: () => void; // Function to toggle theme
+};
+
 // Create a Context for managing theme state globally
-const ThemeContext = createContext<any>(null);
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export const useTheme = () => {
-  return useContext(ThemeContext);
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
 };
 
 const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
@@ -29,7 +37,7 @@ const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) =>
     palette: {
       mode: isDarkMode ? 'dark' : 'light',
       primary: {
-        main: isDarkMode? "#f00" : "#ff0"
+        main: isDarkMode ? "#f00" : "#ff0",
       },
     },
   });
